09e1e8be47ebfc7e5d56045ecc3dbe0cec515ee3
Kyra Bergsund <k.bergsund@gmail.com>
2022-01-06 08:25:55 -0800

    Build component file structure, fetch data & display on DOM

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
+import Reservations from '../Reservations/Reservations';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations: [],
+      error: null
+    }
+  }
+
+  componentDidMount = () => {
+    fetch('http://localhost:3001/api/v1/reservations')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          reservations: data
+        })
+      })
+    // .catch(err => {
+    //   this.setState({
+    //     error: err.message
+    //   })
+    // })
+  }
+
+  
   render() {
+    const displayReservations = this.state.reservations && this.state.reservations.map(resy => <Reservations key={resy.id} name={resy.name} date={resy.date} time={resy.time} number={resy.number} />)
+
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
@@ -10,7 +37,7 @@ class App extends Component {
 
         </div>
         <div className='resy-container'>
-          
+          {displayReservations}
         </div>
       </div>
     )
---
+++ src/Reservations/Reservations.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Reservations = ({ name, date, time, number }) => {
+  return(
+  <section>
+    <h2>{name}</h2>
+    <p>{date}</p>
+    <p>{time}pm</p>
+    <p>Number of guests: {number}</p>
+    <button>Cancel</button>
+  </section>
+  )
+}
+
+export default Reservations;
